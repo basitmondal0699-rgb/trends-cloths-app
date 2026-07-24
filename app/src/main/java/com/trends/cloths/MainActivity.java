4e3b1962d89cd0e2fd334f4525907e17544bc3b1
basitmondal0699-rgb <basitmondal0699@gmail.com>
2026-07-25 03:28:49 +0530

    Add WebView to MainActivity for loading URL
    Implement a WebView to load a remote URL and handle back navigation.

---
+++ app/src/main/java/com/trends/cloths/MainActivity.java
@@ -0,0 +1,35 @@
+package com.trends.cloths;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://trends-cloths.netlify.app/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
